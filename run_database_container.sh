e5e1a4165cc179a8ffa603513d28611650c5783a
CynicalCode21 <cynicaldead@gmail.com>
2021-08-21 12:27:27 +0100

    refac: docker run now makes db_Store in current directory

--- run_database_container.sh
+++ run_database_container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -p 127.0.0.1:3306:3306  --name trees-container -e MARIADB_ROOT_PASSWORD=hawthorn -v ~/Documents/Taynuilt_Trees/db_store:/var/lib/mysql -d mariadb:latest
+sudo docker run -p 127.0.0.1:3306:3306  --name trees-container -e MARIADB_ROOT_PASSWORD=hawthorn -v $(pwd)/db_store:/var/lib/mysql -d mariadb:latest
