65dd4b4c0c526d782964020504eb93139794f704
CynicalCode21 <cynicaldead@gmail.com>
2021-08-21 18:41:53 +0100

    feat: added nginx, now serves up built front-end and *should* proxy the development server to the back-end

--- back_end_trees/index.js
+++ back_end_trees/index.js
@@ -1,1 +1,15 @@
-console.log("bye world");
+const express = require('express')
+const app = express()
+const port = 21451
+
+app.get('/api', (req, res) => {
+  res.send('Hello API!')
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
