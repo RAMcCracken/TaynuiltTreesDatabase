103640f9a01f7378ab2344dc11aecc1e408e4dcd
CynicalCode21 <cynicaldead@gmail.com>
2021-08-22 17:13:42 +0100

    refac: moved commented out code

--- back_end_trees/routes/customer.js
+++ back_end_trees/routes/customer.js
@@ -40,17 +40,6 @@ router.get('/', function (req, res) {
     })
 })
 
-          //  conn.query(`
-          //    INSERT INTO Customer (customer_ref, firstname, surname,
-          //      email, company, address_number, address_street, address_town,
-          //      address_postcode) 
-          //    VALUES(?,?,?,?,?,?,?,?,?)
-          //    `), [c.customer_ref,c.firstname,c.surname,c.email,c.company,c.address_number,c.address_street,c.address_town,c.address_postcode], (err) => {
-            //
-//              conn.batch(`
-//                INSERT INTO Customer_Phone phone_number, customer_ref VALUES (?, ?)
-//                `), c.phone_numbers, (err) => {
-//                  if (err) {
 router.post('/', async function (req, res) {
   let db_pool = req.app.get('db_pool');
   let c = req.body
@@ -96,9 +85,19 @@ router.post('/', async function (req, res) {
   })
 })
 
+// update every field in the Customers table to the given json
 router.put('/', function (req, res) {
   res.send('customer put')
 })
+
+router.delete("/phone_number", function (req, res) {
+
+})
+router.post("/phone_number", function (req, res) {
+
+})
+
+// remove a customer from the database (cascade delete of orders etc?)
 router.delete('/', function (req, res) {
   res.send('customer delete')
 })
