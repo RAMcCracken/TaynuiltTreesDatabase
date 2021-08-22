1b5dc421c6772abbcb67cf64c6f88d24bf1c9ae3
CynicalCode21 <cynicaldead@gmail.com>
2021-08-22 18:38:12 +0100

    feat: added updating customers

--- back_end_trees/routes/customer.js
+++ back_end_trees/routes/customer.js
@@ -86,17 +86,66 @@ router.post('/', async function (req, res) {
 // update every field in the Customers table to the given json
 // also update phone numbers
 router.put('/', function (req, res) {
-  res.send('customer put')
+  let db_pool = req.app.get('db_pool');
+  let c = req.body;
+  let e_msg = "Err: PUT /api/customer -";
+  db_pool.getConnection()
+    .then(conn => {
+      conn.beginTransaction()
+        .then(() => {
+          conn.query(`
+            UPDATE Customer SET firstname = ?, surname = ?, email = ?, company = ?, address_number = ?, address_street = ?, address_town = ?, address_postcode = ?
+            WHERE customer_ref = ?
+          `, [c.firstname,c.surname,c.email,c.company,c.address_number,c.address_street,c.address_town,c.address_postcode,c.customer_ref])
+            .then((rows) => {
+              if (rows.affectedRows > 0) {
+                conn.query(`DELETE FROM Customer_Phone WHERE customer_ref = ?`, [c.customer_ref])
+                  .then(() => {
+                    conn.batch(`
+                    INSERT INTO Customer_Phone (customer_ref, phone_number) VALUES (?, ?)
+                    `, c.phone_numbers)
+                      .then(() => {
+                        console.log()
+                        conn.commit()
+                          .then(() => {
+                            res.json(c);
+                          })
+                          .catch((err) => {
+                            res.status(400).send(`${e_msg} failed to commit transaction\n${err}`);
+                          })
+                      })
+                      .catch((err) => {
+                        console.log(`${e_msg} inserting new phone numbers\n${err}`);
+                        conn.rollback();
+                        res.status(400).send(`${e_msg} inserting new phone numbers\n${err}`);
+                      })
+                  })
+                  .catch((err) => {
+                    console.log(`${e_msg} deleting old phone numbers\n${err}`);
+                    conn.rollback();
+                    res.status(400).send(`${e_msg} deleting old phone numbers\n${err}`);
+                  })
+              } else {
+                  console.log(`${e_msg} updating customer, customer doesn't exist`);
+                  conn.rollback();
+                  res.status(400).send(`${e_msg} updating customer, customer doesn't exist`);
+              }
+            })
+            .catch((err) => {
+                console.log(`${e_msg} updating customer\n${err}`);
+                conn.rollback();
+                res.status(400).send(`${e_msg} updating customer\n${err}`);
+            })
+        })
+        .catch((err) => {
+          console.log(`${e_msg} creating transaction\n${err}`);
+          res.status(400).send(`${e_msg} creating transaction\n${err}`);
+        })
+    })
 })
 
 // remove a customer from the database (cascade delete of orders etc?)
 router.delete('/', function (req, res) {
-  let db_pool = req.app.get('db_pool');
-  let c = req.body
-  db_pool.getConnection()
-    .then(conn => {
-      conn.query(``)
-    })
 })
 
 // export to main js file
