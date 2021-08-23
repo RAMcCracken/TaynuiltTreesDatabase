c2935963f4a6bd2cf19290f0126549e068f48bad
CynicalCode21 <cynicaldead@gmail.com>
2021-08-23 09:22:56 +0100

    fix: actually fix load dump

--- scripts/load_dump.sh
+++ scripts/load_dump.sh
@@ -1,2 +1,2 @@
-sudo docker exec maraidb-container mysql --user root --password=hawthorn -e "CREATE DATABASE IF NOT EXISTS taynuilttrees"
+sudo docker exec mariadb-container mysql --user root --password=hawthorn -e "CREATE DATABASE IF NOT EXISTS taynuilttrees"
 sudo docker exec -i mariadb-container mysql --user root --password=hawthorn taynuilttrees < db.dump
