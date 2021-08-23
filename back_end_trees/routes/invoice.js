6d0d9a62922b261a07a09e7662f72f175d080a8e
CynicalCode21 <cynicaldead@gmail.com>
2021-08-23 18:14:47 +0100

    feat: post invoice

--- back_end_trees/routes/invoice.js
+++ back_end_trees/routes/invoice.js
@@ -9,7 +9,25 @@ router.use(function (req, res, next) {
 // POST new invoice
 router.post('/', function (req, res) {
   let db_pool = req.app.get('db_pool');
+  let i = req.body
   let e_msg = "Err: POST /api/invoice -";
+
+  db_pool.getConnection().then(conn => {
+    conn.query(`
+      INSERT INTO Invoice (invoice_no, invoice_date, discount, vat, payment_method, paid, date_paid, order_no, delivery_ref) VALUES (?,?,?,?,?,?,?,?,?)
+      `,[i.invoice_no,i.invoice_date,i.discount,i.vat,i.payment_method,i.paid,i.date_paid,i.order_no,i.delivery_ref]).then(() => {
+        conn.end();
+        res.send(i);
+      }).catch(err => {
+        conn.end();
+        console.log(`${e_msg} adding invoice\n${err}`)
+        res.status(500).send(`${e_msg} adding invoice\n${err}`)
+      })
+  }).catch(err => {
+    conn.end();
+    console.log(`${e_msg} getting connection from pool\n${err}`)
+    res.status(500).send(`${e_msg} getting connection from pool\n${err}`)
+  })
 })
 
 // PUT update invoice
