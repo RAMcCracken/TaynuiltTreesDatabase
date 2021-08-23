95554df8508eab08853db4933ed117a3c5524a37
CynicalCode21 <cynicaldead@gmail.com>
2021-08-23 12:07:51 +0100

    feat: can now get all orders and all info relating to an individual order

--- back_end_trees/routes/orders.js
+++ back_end_trees/routes/orders.js
@@ -20,7 +20,7 @@ router.use(function (req, res, next) {
 // GET all orders
 router.get('/', function (req, res) {
   let db_pool = req.app.get('db_pool');
-  let e_msg = "Err: GET /api/customer -";
+  let e_msg = "Err: GET /api/order -";
   db_pool.getConnection()
     .then(conn => {
       conn.query(`
@@ -45,7 +45,87 @@ router.get('/', function (req, res) {
 
 // GET a order by :order_ref.
 // Return all ass order_products, invoices, and each invoices ass products.
-// order: {order_products: [], invoices: {[data: foo, invoice_products: []]}}
+// order: {order_products: [], invoices: [data: foo, invoice_products: []]}
+router.get('/:order_no', function (req, res) {
+  let order_no = req.params.order_no;
+  let db_pool = req.app.get('db_pool');
+  let e_msg = `Err: GET /api/order/${order_no} -`;
+  db_pool.getConnection()
+    .then(conn => {
+      conn.query(`
+        SELECT * FROM Orders WHERE order_no = ?
+        `, [order_no])
+        .then(rows => {
+          // orders: {order: {foo}}
+          delete rows.meta;
+          order = {order: rows[0]}
+        })
+          .then(() => {
+            // get ass order prod
+            conn.query(`
+              SELECT * FROM Order_Products WHERE order_no = ?
+              `, [order_no])
+              .then(rows => {
+                // orders: {order: {food}, order_ass_prod: [{foo}]}
+                delete rows.meta;
+                order.order_ass_prod = rows;
+              })
+              .then(() => {
+                conn.query(`
+                  SELECT * FROM Invoice WHERE order_no = ?
+                  `, [order_no])
+                  .then(rows => {
+                    // orders: {order: {food}, order_ass_prod: [{foo}], invoices: [{}]}
+                    delete rows.meta;
+                    order.invoices = rows;
+                  })
+                  .then(() => {
+                    let new_invoices = order.invoices.map(invoice => {
+                      return conn.query(`
+                        SELECT * FROM Invoice_Products WHERE invoice_no = ?
+                        `, [invoice.invoice_no])
+                        .then(rows => {
+                          delete rows.meta;
+                          invoice.invoice_ass_prod = rows
+                          return invoice
+                        })
+                        .catch(err => {
+                          conn.end();
+                          console.log(`${e_msg} getting invoice ass products\n${err}`)
+                          res.status(500).send(`${e_msg} getting invoice ass products\n${err}`)
+                        })
+                    })
+                    // map returns an array of promises that we have to resolve
+                    Promise.all(new_invoices).then(invoices => {
+                      order.invoices = invoices
+                    }).then(() => {
+                      conn.end();
+                      res.send(order)
+                    }).catch((err) => {
+                      conn.end();
+                      console.log(`${e_msg} getting order\n${err}`)
+                      res.status(500).send(`${e_msg} getting order\n${err}`)
+                    })
+                  })
+              })
+              .catch(err => {
+                conn.end();
+                console.log(`${e_msg} getting invoices\n${err}`)
+                res.status(500).send(`${e_msg} getting invoices\n${err}`)
+              })
+          })
+        .catch(err => {
+          conn.end();
+          console.log(`${e_msg} getting orders\n${err}`)
+          res.status(500).send(`${e_msg} getting orders\n${err}`)
+        })
+      })
+      .catch(err => {
+            conn.end();
+            console.log(`${e_msg} getting connection from pool\n${err}`)
+            res.status(500).send(`${e_msg} getting connection from pool\n${err}`)
+      })
+})
 
 
 
